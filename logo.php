371b5d58c3c835fadf5a79ebacac6ae4c56ece0e
Miliano <jrmfilho23@gmail.com>
2019-03-22 22:36:46 -0300

    ..

--- logo.php
+++ logo.php
@@ -1,19 +1,23 @@
 <?php
 
-$handle = fopen("https://laravel.com/", "r");
-$fp = fopen('data.blade.php', 'w');
 
-while (!feof($handle)) {
-   $buffer = fgets($handle);
-   echo $buffer; 
-   fwrite($fp, $buffer);
+                                               //  fopen abre um arquivo ou url
+$start = microtime(true);                      // Incio da contagem do tempo de execução
+$handle = fopen("https://laravel.com/", "r");  // Cria um objeto de leitura laravel
+$fp = fopen('data.blade.php', 'w');            //criar um objeto de leitura e escrita
+
+                                               // feof verifica quando o $handle chega ao fim do seu armazenamento: limit 8K
+while (!feof($handle)) {                       //loop para armazenar imprimir conteudo no arquivo data.blade.php
+   $buffer = fgets($handle);                   // fegts lê um ponteiro de arquivo
+   fwrite($fp, $buffer);                       // escreve o conteudo da variavel $buffer no arquivo $fp
 }
 
-fclose($fp);
-fclose($handle);
+fclose($fp);                                   // close the file 
+fclose($handle);                               // close the file
 
-
-
+$end = microtime(true);                        //termina a contagem
+$tempo = $end - $start;                        //processo
+echo $tempo;                                   //imprime a contagem
 
 
 
